cbee2fff87c641af133dbb3d5a876c5fa2cee693
kelvenbrito <kelvenbrito14@gmail.com>
2024-02-29 00:57:32 -0300

    29/02/24

---
+++ hello_world/src/main/java/webapp/hello_world/Controller/indexController.java
@@ -0,0 +1,35 @@
+package webapp.hello_world.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/** @author Rolfi Luz - Senai * */
+@Controller
+public class indexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView abrirIndex() {
+        ModelAndView mv = new ModelAndView("index");
+
+        String mensagem = "Seja bem-vinda(o)!";
+        mv.addObject("msg", mensagem);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView nomeIndex(@RequestParam("nome") String nome, String end, String date) {
+        ModelAndView mv = new ModelAndView("index");
+
+        String mensagem = "Seja bem-vinda(o)! !";
+        mv.addObject("msg", mensagem);
+        mv.addObject("nome", nome);
+        mv.addObject("end", end);
+        mv.addObject("date", date);
+
+
+        return mv;
+    }
+}
