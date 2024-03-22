554e2824063e7fc306b9a03b44d92bca095519e5
kelvenbrito <kelvenbrito14@gmail.com>
2024-03-21 22:00:14 -0300

    21/03/24

---
+++ crud_escola/src/main/java/webapp/crud_escola/Repository/VerificaCadastroAdmRepository.java
@@ -0,0 +1,9 @@
+package webapp.crud_escola.Repository;
+import org.springframework.data.repository.CrudRepository;
+import webapp.crud_escola.Model.VerificaCadastroAdm;
+
+public interface VerificaCadastroAdmRepository extends CrudRepository<VerificaCadastroAdm, String>{
+    VerificaCadastroAdm findByCpf (String cpf);
+    VerificaCadastroAdm findByNome(String nome);
+    
+} 
